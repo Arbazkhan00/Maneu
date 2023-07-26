dc5af9f0026422f4dafc97f41faa7075f42f828e
Arbaz Khan <arbazjani8@gmail.com>
2023-07-26 15:56:19 -0700

    commit

--- app/components/ui/Nav/Nav.tsx
+++ app/components/ui/Nav/Nav.tsx
@@ -8,21 +8,21 @@ import Logo from "@/app/components/assest/images/Logo.webp"
 
 const Nav = () => {
   return (
-    <div className='px-36 py-10 text-center items-center'>
-        <header className=' flex gap-20' >
-        <div className=' absolute xs:flex-col '>
+    <div className='px-36 py-10 text-center items-center xs:flex-col'>
+        <header className=' flex gap-20 xs:flex-col' >
+        <div className=' absolute xs:flex-col xs:items-center xs:text-center'>
                 <Image src={Logo} alt='Logo' />
               </div>
             <nav className='flex gap-14 xs:flex-col' >
               
-                <ul className='flex gap-10 ml-56 xs:flex-col xs:text-center'>
+                <ul className='flex gap-10  xs:flex-col xs:text-center xs:mr-96 xs:mt-14 '>
                     <li>Male</li>
                     <li>Female</li>
                     <li>Kids</li>
                     <li>All Products</li>
                     </ul>
                    
-                    <div className='border-gray-500  flex ml-8 xs:flex-col'>
+                    <div className='border-gray-500  flex  xs:flex-col xs:mr-20'>
                     
                     <BiSearchAlt size={23}/>
                     
@@ -30,7 +30,7 @@ const Nav = () => {
                         <input type="search" placeholder='What you looking for' width={54}  className=' text-center '/>
                     </div>
 
-                    <div className='xs:flex-col ml-8'>
+                    <div className='xs:flex-col '>
                       <span className='absolute  text-red-600 font-semibold text-xl rounded-full w-5 h-5 pt-5 text-center top-0 '>0</span>
                       
                         <AiOutlineShopping size={28} />
