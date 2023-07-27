df4ac36066be12a75a3b604e63af8b579764c56a
Arbaz Khan <arbazjani8@gmail.com>
2023-07-27 07:59:13 -0700

    maneu

--- app/components/ui/Nav/Nav.tsx
+++ app/components/ui/Nav/Nav.tsx
@@ -5,42 +5,33 @@ import {BiSearchAlt} from "react-icons/bi"
 import { Badge } from "@/components/ui/badge"
 
 import Logo from "@/app/components/assest/images/Logo.webp"
+import { headers } from 'next/dist/client/components/headers'
 
 const Nav = () => {
   return (
-    <div className='px-36 py-10 text-center items-center xs:flex-col'>
-        <header className=' flex gap-20 xs:flex-col' >
-        <div className=' absolute xs:flex-col xs:items-center xs:text-center'>
-                <Image src={Logo} alt='Logo' />
-              </div>
-            <nav className='flex gap-14 xs:flex-col' >
-              
-                <ul className='flex gap-10  xs:flex-col xs:text-center xs:mr-96 xs:mt-14 '>
-                    <li>Male</li>
-                    <li>Female</li>
-                    <li>Kids</li>
-                    <li>All Products</li>
-                    </ul>
-                   
-                    <div className='border-gray-500  flex  xs:flex-col xs:mr-20'>
-                    
-                    <BiSearchAlt size={23}/>
-                    
-                   
-                        <input type="search" placeholder='What you looking for' width={54}  className=' text-center '/>
-                    </div>
+   <header className='bg-gray-500'>
+    <nav className='flex justify-between items-center w-[92%] mx-auto'>
+      <div>
+        <Image src={Logo} alt='...'/>
+      </div>
 
-                    <div className='xs:flex-col '>
-                      <span className='absolute  text-red-600 font-semibold text-xl rounded-full w-5 h-5 pt-5 text-center top-0 '>0</span>
-                      
-                        <AiOutlineShopping size={28} />
 
-                      
-                    </div>
-            </nav>
-            
-        </header>
-    </div>
+      <div className='static  min-max-h-fit xs:absolute w-auto  min-[60vh] left-0 top-[-100%] sx:w-full flex items-center px-5' >
+        <ul className='flex md:flex-row xs:flex-col   md:items-center gap-[4vw] xs:gap-8'>
+          <li>Female</li>
+          <li  >Male</li>
+          <li>Kids</li>
+          <li>All Products</li>
+        </ul>
+      </div>
+
+
+      <div><input type="search" name="" id="" placeholder='what you looking  for' /></div>
+
+      <div><AiOutlineShopping size="32"/></div>
+
+    </nav>
+   </header>
   )
 }
 
